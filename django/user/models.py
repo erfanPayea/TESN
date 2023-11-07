0bc7dabb38a73b8f1c2eed890f17603a7119cce4
ruzbeh <ruzbeh.moini@gmail.com>
2023-11-07 22:22:28 +0330

    user models added

---
+++ django/user/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here
+class User(AbstractUser):
+    phone = models.CharField(max_length=13)
+    test = models.CharField(max_length=256)
+    birth_date = models.DateField(null=True)
+    date_joined = models.DateField(auto_now=True)
+
+
+class Followers(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followings')
+    following_date = models.DateField(auto_now=True)
+
+# class CityFollowings(models.Model):
+#     follower = models.ForeignKey(User, on_delete=models.CASCADE)
+#     following = models.ForeignKey(City, on_delete=models.CASCADE)
+#     following_date = models.DateField(auto_now=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
