6bf1bc53bbdebc57edbdd1c21392083b6e4a8d82
Payea <mmdPayea@gmail.com>
2023-12-30 18:31:49 +0330

    related_name added to all experience models

--- project/experience/models.py
+++ project/experience/models.py
@@ -11,12 +11,12 @@ class City(models.Model):
 class Attraction(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
-    city = models.ForeignKey(City, null=True, on_delete=models.SET_NULL)
+    city = models.ForeignKey(City, null=True, on_delete=models.SET_NULL, related_name="attractions")
     path = models.CharField(max_length=50)
 
 
 class Experience(models.Model):
-    owner = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name="posts")
     attraction = models.ForeignKey(Attraction, null=True, on_delete=models.SET_NULL)
     sent_time = models.DateTimeField(auto_now=True)
     file_path = models.CharField(max_length=50, null=True)
@@ -33,30 +33,30 @@ class Post(Experience):
 
 class Review(Experience):
     owner = models.ForeignKey(user_models.User, null=True, on_delete=models.SET_NULL)
-    attraction = models.ForeignKey(Attraction, null=False, on_delete=models.CASCADE)
+    attraction = models.ForeignKey(Attraction, null=False, on_delete=models.CASCADE, related_name="reviews")
 
 
 class Comment(models.Model):
     owner = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    destination_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    destination_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     sent_time = models.DateTimeField(auto_now=True)
     number_of_likes = models.PositiveIntegerField()
 
 
 class Like(models.Model):
-    owner = models.ForeignKey(user_models.User,null=True, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(user_models.User, null=True, on_delete=models.SET_NULL)
 
     class Meta:
         abstract = True
 
 
 class LikePost(Like):
-    destination_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    destination_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
 
 
 class LikeReview(Like):
-    destination_review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    destination_review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="likes")
 
 
 class LikeComment(Like):
-    destination_comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    destination_comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="likes")
