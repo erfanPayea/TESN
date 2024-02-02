2f8e2867d67e3ec766f4fa928202fedf1964c200
ruzbeh <ruzbeh.moini@gmail.com>
2024-02-02 23:37:55 +0330

    minor changes

--- project/user/serializers.py
+++ project/user/serializers.py
@@ -6,7 +6,27 @@ from . import models
 class Follower(ModelSerializer):
     class Meta:
         model = models.Followers
-        fields = '__all__'
+        fields = (
+            "id",
+            "following_date",
+        )
+
+    def to_representation(self, instance):
+        request = self.context.get('request', None)
+        current_user = getattr(request, 'user', None)
+        data = super().to_representation(instance)
+        if instance.follower == current_user:
+            other_user = instance.following
+        elif instance.following == current_user:
+            other_user = instance.follower
+        else:
+            return data
+        data['cantact']= {
+                'id': other_user.id,
+                'username': other_user.username,
+                # Add other user fields as needed
+            }
+        return data
 
 
 class UserSerializer(ModelSerializer):
